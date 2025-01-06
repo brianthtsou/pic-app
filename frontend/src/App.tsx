7319c9f480055f4a8ae8adf980958ddb95a6f752
brianthtsou <95939519+brianthtsou@users.noreply.github.com>
2025-01-05 18:03:01 -0800

    complete basic login form ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,38 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<EventTarget>): void => {
+    event.preventDefault();
+    alert(password);
+  };
+
   return (
     <>
-      <div className="login-container">
+      <div className="login-title">
         <h1>Login</h1>
-        <label htmlFor="username-input-field">Username:</label>
-        <input name="username-input-field"></input>
-        <label htmlFor="password-input-field">Password:</label>
-        <input name="password-input-field"></input>
+      </div>
+      <div className="login-container">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username-input-field">Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            name="username-input-field"
+          ></input>
+          <label htmlFor="password-input-field">Password:</label>
+          <input
+            name="password-input-field"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </>
   );
